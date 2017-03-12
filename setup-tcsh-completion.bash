f70f800667923128896cdd676507bd3ab67dacbb
Marc Khouzam <marc.khouzam@ericsson.com>
2017-03-12 12:07:34 -0400

    Using npm for simpler installation
    Even though this is not an node package,
    so many people understand npm that we can use
    it as a way to run the installation for tcsh-completion.

---
+++ setup-tcsh-completion.bash
@@ -0,0 +1,103 @@
+#!/bin/bash -i
+#
+# Copyright (C) 2017 Marc Khouzam <marc.khouzam@gmail.com>
+# Distributed under the MIT License (MIT)
+#
+# When sourced, this script will generate a new script that uses
+# the git-completion.bash script provided by core Git.  This new
+# script can be used by tcsh to perform git completion.
+# The current script also issues the necessary tcsh 'complete'
+# commands.
+#
+# To use this completion script:
+#
+#    0) You need tcsh 6.16.00 or newer.
+#    1) Copy both this file and the bash completion script to ${HOME}.
+#       You _must_ use the name ${HOME}/.git-completion.bash for the
+#       bash script.
+#       (e.g. ~/.git-completion.tcsh and ~/.git-completion.bash).
+#    2) Add the following line to your .tcshrc/.cshrc:
+#        source ~/.git-completion.tcsh
+#    3) For completion similar to bash, it is recommended to also
+#       add the following line to your .tcshrc/.cshrc:
+#        set autolist=ambiguous
+#       It will tell tcsh to list the possible completion choices.
+
+root_path="`dirname $0`"
+setup_location="${root_path}/completions"
+completion_file="${setup_location}/tcsh-complete-commands.tcsh"
+
+# Check that tcsh is modern enough for completion
+tcsh_version=(`tcsh --version | awk '{print $2}' | \sed 's/\./ /g'`)
+if [[ ${tcsh_version[0]} -lt 6 || \
+     ( ${tcsh_version[0]} -eq 6 && \
+       ${tcsh_version[1]} -lt 16 ) ]]; then
+  unset tcsh_version
+  echo "ERROR: Your version of tcsh is too old, you need version 6.16.00 or newer.  Enhanced tcsh completion will not work."
+  exit
+fi
+
+# Echo the tcsh 'complete' command corresponding
+# to the script passed as a parameter.
+# Parameters:
+# 1: The full path of the bash completion script
+#    for which this method will generate a tcsh
+#    'complete' command.
+_generate_tcsh_complete_command ()
+{
+  toolCompletionScript=$1
+
+  # It does not look like the main bash completion script is necessary
+  # to generate the 'complete' commands we are interested in.
+  # So let's avoid to source it to save time
+  #
+  #bashCompletionScript=/usr/share/bash-completion/bash_completion
+  #if [ -e ${bashCompletionScript} ]; then
+  #  source ${bashCompletionScript}
+  #fi
+
+  # Remove any existing complete commands
+  complete -r
+
+  # Source the script to generate the complete command(s) we will parse
+  # Note that for some of the scripts we may handle to properly be sourced,
+  # we need to run the bash shell in interactive mode; this explains why
+  # we must use the '-i' flag at the top of the file.
+  # For example, this is necessary for the 'apropos' completion script
+  source ${toolCompletionScript} &> /dev/null
+
+  # Read each complete command generated as long as uses the -F format
+  complete | egrep -e '-F' | while read completionCommand
+  do
+    # Remove everything up to the last space to find the command name
+    # e.g. complete -o bashdefault -o default -o nospace -F git_wrapgitk_main gitk
+    #  becomes
+    #      gitk
+    commandName=${completionCommand##* }
+
+    # Remove everything up to and including "-F "
+    # e.g. complete -o bashdefault -o default -o nospace -F git_wrapgitk_main gitk
+    #  becomes
+    #      git_wrapgitk_main gitk
+    tmp=${completionCommand##*-F }
+    # Remove everyting after the first space
+    # e.g. git_wrapgitk_main gitk
+    #  becomes
+    #  git_wrapgitk_main
+    # Note that we cannot simply use the output in $tmp to
+    # express both strings we are looking for as there could
+    # be other parameters included in $tmp
+    commandFunction=${tmp%% *}
+
+    echo complete ${commandName} \'p,\*,\`bash\ ${HOME}/.tcsh-completion.bash\ ${commandFunction}\ ${toolCompletionScript}\ \"\$\{COMMAND_LINE\}\"\`,\'
+  done
+}
+
+# Go over each bash completion script and generate a corresponding 'complete' command
+\mkdir -p "${setup_location}"
+for script_path in /usr/share/bash-completion/completions/*; do
+  _generate_tcsh_complete_command "${script_path}" >> "${completion_file}"
+done
+
+# Don't include those more basic completions until the tcsh handling is more robust
+#  _generate_tcsh_complete_command /usr/share/bash-completion/bash_completion >> "${completion_file}"
